50574739eed7027f379f334febad8334d013b997
Samuel Robinson <samueljzrobinson@gmail.com>
2025-07-24 21:57:52 +1000

    Basic Page Layout

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <div id="logo-container">
+        <a href="#">
+          <img src="#" />
+          <p className="title">Travel King</p>
+        </a>
+      </div>
+    </header>
+  );
+}
---
+++ my-app/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+export default function MainContent() {
+  return (
+    <main>
+      <div class="entry-container"></div>
+      <p>asjdisdj</p>
+    </main>
+  );
+}
