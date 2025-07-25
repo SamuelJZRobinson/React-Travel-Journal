5c1d0dcc750514bc58398a1b6fd5d05f130309e3
Samuel Robinson <samueljzrobinson@gmail.com>
2025-07-25 15:37:35 +1000

    Import Data Props

--- my-app/src/components/MainContent.jsx
+++ my-app/src/components/MainContent.jsx
@@ -1,35 +1,21 @@
 import Entry from "./Entry";
+import tourData from "../data/tourData";
 
 export default function MainContent() {
   return (
     <main>
       <div className="entry-container">
-        <Entry
-          img={{ src: "./src/assets/paris.jpg", alt: "Eiffel Tower" }}
-          country={"Paris"}
-          location={"Eiffel Tower"}
-          mapURL={
-            "https://www.google.com/maps/place/Eiffel+Tower/@48.8580746,2.2925508,691m/data=!3m1!1e3!4m10!1m2!2m1!1seiffel+tower+map!3m6!1s0x47e66e2964e34e2d:0x8ddca9ee380ef7e0!8m2!3d48.8583701!4d2.2944813!15sChBlaWZmZWwgdG93ZXIgbWFwWhIiEGVpZmZlbCB0b3dlciBtYXCSARNoaXN0b3JpY2FsX2xhbmRtYXJrmgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5UVU52YVU5dWVUbDNSUkFCqgFVCggvbS8wMmo4MQoIL20vMDRfdGIQASoHIgNtYXAoADIeEAEiGvA6UK1e_GAfYYg3fmoLnSiwFo9cbzeHxnPEMhQQAiIQZWlmZmVsIHRvd2VyIG1hcOABAPoBBAgAEBs!16zL20vMDJqODE?entry=ttu&g_ep=EgoyMDI1MDcyMS4wIKXMDSoASAFQAw%3D%3D"
-          }
-          startDate="24 July 2025"
-          endDate="8 August 2025"
-          description={
-            "The Eiffel Tower is a wrought-iron lattice tower in Paris, France. Built in 1889, it stands 330 meters tall and is an iconic symbol of French architecture and engineering."
-          }
-        />
-        <Entry
-          img={{ src: "./src/assets/vietnam.jpg", alt: "Boats on water" }}
-          country={"Vietnam"}
-          location={"Hoi"}
-          mapURL={
-            "https://www.google.com/maps/place/Ho%C3%A0i+T%C3%A2n,+Binh+Dinh,+Vietnam/@14.4709516,108.9314415,16283m/data=!3m2!1e3!4b1!4m15!1m8!3m7!1s0x31157a4d736a1e5f:0xb03bb0c9e2fe62be!2sVietnam!3b1!8m2!3d14.058324!4d108.277199!16zL20vMDFjcmQ1!3m5!1s0x3168c4d7f4689d05:0x3d32f2fda146b4b9!8m2!3d14.4756838!4d109.0037125!16s%2Fg%2F11b7xq0zqh?entry=ttu&g_ep=EgoyMDI1MDcyMS4wIKXMDSoASAFQAw%3D%3D"
-          }
-          startDate="27 July 2025"
-          endDate="18 August 2025"
-          description={
-            "Hoi An is a historic Vietnamese city along the Thu Bon River, known for its ancient town, lantern-lit nights, canals, traditional architecture, and vibrant markets, offering a serene riverside charm."
-          }
-        />
+        {tourData.map((item) => (
+          <Entry
+            img={item.img}
+            country={item.country}
+            location={item.location}
+            mapURL={item.mapURL}
+            startDate={item.startDate}
+            endDate={item.endDate}
+            description={item.description}
+          />
+        ))}
       </div>
     </main>
   );
